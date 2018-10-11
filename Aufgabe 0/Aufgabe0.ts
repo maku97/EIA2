1f8737826b339147e1789837e81d1f7301edd93e
Marius <Marius@Marius-Acer.eduroam.hs-furtwangen.de>
2018-10-11 11:07:20 +0200

    verbessert

--- Aufgabe 0/Aufgabe0.ts
+++ Aufgabe 0/Aufgabe0.ts
@@ -3,7 +3,7 @@
     var name:string ="";
     function box(){
         var name = prompt ("Wie heiﬂt du?");
-        var node:any= document.getElementById("content");
+        var node:HTMLElement= document.getElementById("content");
         node.innerHTML += "Herzlich Willkommen ";
         node.innerHTML += name;
         node.innerHTML += "!";
